b290947f96674fab9324931904d8d9776aa73c25
Beatriz Coimbra <beatrizmcoimbra@outlook.com>
2016-12-13 17:02:56 -0200

    Update [FUNCTION] flip-horiz.rkt

--- [FUNCTION] flip-horiz.rkt
+++ [FUNCTION] flip-horiz.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(define (flip-horizontal painter)
+(define (flip-horiz painter)
    ((transform-painter (make-vect 1.0 0.0)
                        (make-vect 0.0 0.0)
                        (make-vect 1.0 1.0))
