8236933c628128544a0d8e60f93674fe22dbef80
Beatriz Coimbra <beatrizmcoimbra@outlook.com>
2016-12-13 17:02:21 -0200

    adicionando a função flip-horiz

--- Implementação dos códigos do livro.rkt
+++ Implementação dos códigos do livro.rkt
@@ -131,3 +131,11 @@
       (lambda (frame)
         (paint-left frame)
         (paint-right frame)))))
+       
+(define (flip-horiz painter)
+   ((transform-painter (make-vect 1.0 0.0)
+                       (make-vect 0.0 0.0)
+                       (make-vect 1.0 1.0))
+    painter))
+        
+        
